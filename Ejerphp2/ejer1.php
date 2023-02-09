681a00862438ad2746027057e58759dfc8fbbabf
afragarcab <a_fragarcab@iestriana.com>
2023-02-09 10:17:37 +0100

    practicando con el pdf de introduccion a la programacion 2

---
+++ Ejerphp2/ejer1.php
@@ -0,0 +1,16 @@
+<?php
+/*Escribe un programa que lea un número desde el teclado y escriba en pantalla si es
+positivo, negativo o nulo.*/
+
+$num = readline("Introduce número positivo o negativo: ");
+
+if ($num >= 1)
+{
+    echo "¡El número es positivo!";
+} else if ($num <= -1)
+{
+    echo "¡El número es negativo!";
+} else
+{
+    echo "EL número es nulo";
+}
---
+++ Ejerphp2/ejer2.php
@@ -0,0 +1,12 @@
+<?php
+    /*Escribe un programa que lea dos números y escriba el valor de su producto y el de su
+división.*/
+
+$num1 = readline("Introduce un número: ");
+$num2 = readline("Introduce otro número: ");
+
+$multiplicar = $num1 * $num2;
+$division = $num1 / $num2;
+
+echo "El producto es: ".$multiplicar."\n";
+echo "La división es: ".$division;
